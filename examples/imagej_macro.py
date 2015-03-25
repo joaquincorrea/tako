392254964d7f67b6b7cc72e42cc511122107555a
Joaquin Correa <JoaquinCorrea@lbl.gov>
2015-03-25 13:03:15 -0700

    Updates to examples/imagej_macro.py template

--- examples/imagej_macro.py
+++ examples/imagej_macro.py
@@ -3,10 +3,10 @@ __author__ = 'jcorrea'
 from tako.arms.correction import Correction
 from tako.head import head
 
-alignment = Correction(setup={"algorithm": "ijmacro",
+correction = Correction(setup={"algorithm": "ijmacro",
                    "data": "/Users/DOE6903584/NERSC/tako/examples/data/Lenna.png",
                    "params": {'macro': "/Users/DOE6903584/NERSC/tako/bin/correction/imagej-macro.ijm"}
                    }
                       )
 
-do = head.do_workflow(setup=[alignment])
+do = head.do_workflow(setup=[correction])
--- examples/small_workflow.py
+++ examples/small_workflow.py
@@ -16,12 +16,7 @@ from tako.head import head
 alignment = Alignment(setup={"algorithm": "method1",
                    "data": "/Users/DOE6903584/NERSC/tako/examples/data/Lenna.png",
                    "params": {'macro': "/Users/DOE6903584/NERSC/tako/bin/alignment/imagej-macro.ijm"}
-                   # "params": ["--headless", "-macro", "/Users/DOE6903584/NERSC/tako/bin/alignment/imagej-macro.ijm"]})
                    }
                       )
 
 do = head.do_workflow(setup=[alignment])
-
-# do = head.do_workflow()
-#uuuuu
-# head.do_workflow(alignment)
